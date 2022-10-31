1506526b51b8250e6e16f3448233a8c66316cf1d
Ilmagita Nariswari <ilmagita@gmail.com>
2022-10-31 20:19:15 +0700

    Added boolean.h and ADT Cooked

---
+++ src/dinerdash/cooked.h
@@ -0,0 +1,73 @@
+/* File : q_dinerdash.h */
+/* Definisi ADT Queue dengan representasi array secara eksplisit dan alokasi statik */
+
+#ifndef COOKED_H
+#define COOKED_H
+
+#include "boolean.h"
+
+#define IDX_UNDEF -1
+#define CAPACITY 7
+
+/* Definisi elemen dan address */
+
+typedef struct {
+    char* makanan;
+    int durasi;
+} Cooked;
+
+typedef struct {
+	Cooked buffer[CAPACITY]; 
+	int idxHead;
+	int idxTail;
+} QueueCooked;
+
+/* ********* AKSES (Selektor) ********* */
+/* Jika q adalah Queue, maka akses elemen : */
+#define IDX_HEAD(q) (q).idxHead
+#define IDX_TAIL(q) (q).idxTail
+#define     HEAD(q) (q).buffer[(q).idxHead]
+#define     TAIL(q) (q).buffer[(q).idxTail]
+
+/* *** Kreator *** */
+void CreateQueueCooked(QueueCooked *q);
+/* I.S. sembarang */
+/* F.S. Sebuah q kosong terbentuk dengan kondisi sbb: */
+/* - Index head bernilai IDX_UNDEF */
+/* - Index tail bernilai IDX_UNDEF */
+/* Proses : Melakukan alokasi, membuat sebuah q kosong */
+
+/* ********* Prototype ********* */
+boolean isEmptyCooked(QueueCooked q);
+/* Mengirim true jika q kosong: lihat definisi di atas */
+boolean isFullCooked(QueueCooked q);
+/* Mengirim true jika tabel penampung elemen q sudah penuh */
+/* yaitu IDX_TAIL akan selalu di belakang IDX_HEAD dalam buffer melingkar*/
+
+int length(QueueCooked q);
+/* Mengirimkan banyaknya elemen queue. Mengirimkan 0 jika q kosong. */
+
+/* *** Primitif Add/Delete *** */
+void enqueueCooked(QueueCooked *q, Cooked val);
+/* Proses: Menambahkan val pada q dengan aturan FIFO */
+/* I.S. q mungkin kosong, tabel penampung elemen q TIDAK penuh */
+/* F.S. val menjadi TAIL yang baru, IDX_TAIL "mundur" dalam buffer melingkar. */
+
+void dequeueCooked(QueueCooked *q, Cooked *val);
+/* Proses: Menghapus val pada q dengan aturan FIFO */
+/* I.S. q tidak mungkin kosong */
+/* F.S. val = nilai elemen HEAD pd I.S., IDX_HEAD "mundur";
+        q mungkin kosong */
+
+/* *** Display Queue *** */
+void displayQueueCooked(QueueCooked q);
+/* Proses : Menuliskan isi Queue dengan traversal, Queue ditulis di antara kurung 
+   siku; antara dua elemen dipisahkan dengan separator "koma", tanpa tambahan 
+   karakter di depan, di tengah, atau di belakang, termasuk spasi dan enter */
+/* I.S. q boleh kosong */
+/* F.S. Jika q tidak kosong: [e1,e2,...,en] */
+/* Contoh : jika ada tiga elemen bernilai 1, 20, 30 akan dicetak: [1,20,30] */
+/* Jika Queue kosong : menulis [] */
+
+
+#endif
