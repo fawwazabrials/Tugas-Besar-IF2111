a36a65a5e844cf9da56ca4f50ebf456204cf5024
Fawwaz Abrial Saffa <fawwazabrial.fas@gmail.com>
2022-11-04 10:38:39 +0700

    finished START command

--- main.c
+++ main.c
@@ -2,35 +2,47 @@
 #include <stdlib.h>
 #include "src/ADT/mesinkarakter.h"
 #include "src/ADT/mesinkata.h"
+#include "src/ADT/array.h"
 #include "src/functions.h"
 
 int main() {
     // KAMUS LOKAL
     Word masukan;
+    TabWord games;
     boolean loop = true;
     int i, len;
 
     // ALGORITMA
+    MakeEmpty(&games);
     while (loop) {
         input(&masukan);
+
+        // INPUT ADALAH START
         if (isKataEqual(masukan, "START")) {
             loop = false;
             STARTWORDFILE("config.txt");
             len = katatoint(currentWord);
-            for (i=0; i<len; i++) {
+            for (i=1; i<=len; i++) {
                 ADV();
                 ADVWORDFILE();
-                // insert save here
+                SetEl(&games, i, currentWord);
             }
 
-            printf("\nDONE START\n");
+            // goto menu kalo udah dibuat
+            // menu(games, queue_games);
         }
+
+        // INPUT ADALAH LOAD
         else if (isKataEqual(masukan, "LOAD")) {
             printf("LOAD\n");
+            ADVWORD();
+            displayWord(currentWord);   // nama file yang bakal dibaca
             loop = false;
-        } 
+        }
+
+        // INPUT GAJELAS 
         else {
-            printf("OTHERS\n");
+            printf("MASUKAN SALAH!\n");
         }
     }
     
