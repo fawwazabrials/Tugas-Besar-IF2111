94a6b56f150be5a4cba9c1c23234315b57a7069f
Gibran Fasha Ghazanfar <18221069@std.stei.itb.ac.id>
2022-11-04 16:38:49 +0700

    ketuker kayanya

--- src/commands.c
+++ src/commands.c
@@ -1,3 +1,26 @@
 #include "commands.h"
 
-void QUEUEGAME(TabWord games, Queue* game_queue);
+void QUEUEGAME(TabWord games, Queue* game_queue) {
+    int game_idx;
+
+    printf("Berikut adalah daftar antrian game-mu\n");
+    DisplayQueue(*game_queue);
+    printf("\n");
+    printf("Berikut adalah daftar game yang tersedia\n");
+    DisplayArray(games);
+    printf("\n");
+
+    printf("Nomor game yang mau ditambahkan ke antrian: ");
+    STARTWORD();
+    game_idx = katatoint(currentWord);
+
+    if (game_idx <= GetLastIdx(games) && game_idx >= GetFirstIdx(games)) {
+        if (isFull(*game_queue)) printf("Daftar antrian game-mu sudah penuh! Silahkan skip game atau mainkan game terlebih dahulu.\n");
+        else {
+            enqueue(game_queue, GetElmt(games, game_idx));
+            printf("Game berhasil ditambahkan kedalam daftar antria.n\n");
+        }
+    } else {
+        printf("Nomor permainan tidak valid, silahkan masukkan nomor game pada list.\n");
+    }
+}
