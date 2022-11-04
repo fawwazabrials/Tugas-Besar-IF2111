ec940fd9ce7acc05cd2ddcf7c56b4b9391e0c621
Fawwaz Abrial Saffa <fawwazabrial.fas@gmail.com>
2022-11-04 13:28:53 +0700

    added queue

---
+++ src/ADT/queue.c
@@ -0,0 +1,92 @@
+#include "queue.h"
+
+/* *** Kreator *** */
+void CreateQueue(Queue *q) {
+    // KAMUS LOKAL
+
+    // AGLORITMA
+    IDX_HEAD(*q) = IDX_UNDEF;
+    IDX_TAIL(*q) = IDX_UNDEF;
+}
+/* I.S. sembarang */
+/* F.S. Sebuah q kosong terbentuk dengan kondisi sbb: */
+/* - Index head bernilai IDX_UNDEF */
+/* - Index tail bernilai IDX_UNDEF */
+/* Proses : Melakukan alokasi, membuat sebuah q kosong */
+
+/* ********* Prototype ********* */
+boolean isEmpty(Queue q) {
+    // KAMUS LOKAL
+
+    // AGLORITMA
+    return ((IDX_HEAD(q) == IDX_UNDEF) && (IDX_TAIL(q) == IDX_UNDEF));
+}
+/* Mengirim true jika q kosong: lihat definisi di atas */
+boolean isFull(Queue q) {
+    // KAMUS LOKAL
+
+    // AGLORITMA
+    return (length(q) == CAPACITY);
+}
+/* Mengirim true jika tabel penampung elemen q sudah penuh */
+/* yaitu IDX_TAIL akan selalu di belakang IDX_HEAD dalam buffer melingkar*/
+
+int length(Queue q) {
+    // KAMUS LOKAL
+
+    // AGLORITMA
+    if (isEmpty(q)) return 0;
+    else if(IDX_TAIL(q) >= IDX_HEAD(q)) return IDX_TAIL(q) - IDX_HEAD(q) +1;
+    else return IDX_TAIL(q) + 1 + (CAPACITY - IDX_HEAD(q));
+}
+/* Mengirimkan banyaknya elemen queue. Mengirimkan 0 jika q kosong. */
+
+/* *** Primitif Add/Delete *** */
+void enqueue(Queue *q, ElTypeQueue val) {
+    // KAMUS LOKAL
+
+    // AGLORITMA
+    if (isEmpty(*q)) {
+        IDX_HEAD(*q) = 0;
+        IDX_TAIL(*q) = 0;  
+    } else {
+        IDX_TAIL(*q) = (IDX_TAIL(*q) + 1) % CAPACITY;
+    }
+
+    copyWord(val, &TAIL(*q));
+    // TAIL(*q) = val;
+}
+/* Proses: Menambahkan val pada q dengan aturan FIFO */
+/* I.S. q mungkin kosong, tabel penampung elemen q TIDAK penuh */
+/* F.S. val menjadi TAIL yang baru, IDX_TAIL "mundur" dalam buffer melingkar. */
+
+void dequeue(Queue *q, ElTypeQueue *val) {
+    // KAMUS LOKAL
+
+    // AGLORITMA
+    *val = HEAD(*q);
+
+    if (length(*q) == 1) {
+        IDX_HEAD(*q) = IDX_UNDEF;
+        IDX_TAIL(*q) = IDX_UNDEF;  
+    } else {
+        IDX_HEAD(*q) = (IDX_HEAD(*q) + 1) % CAPACITY;
+    }
+}
+/* Proses: Menghapus val pada q dengan aturan FIFO */
+/* I.S. q tidak mungkin kosong */
+/* F.S. val = nilai elemen HEAD pd I.S., IDX_HEAD "mundur";
+        q mungkin kosong */
+
+/* *** Display Queue *** */
+void DisplayQueue(Queue q) {
+    // KAMUS LOKAL
+    int i, val;
+
+    // AGLORITMA
+
+    for (i = 0; i<length(q); i++) {
+        printf("%d. ", i+1);
+        displayWord(q.buffer[i]);
+    }
+}
