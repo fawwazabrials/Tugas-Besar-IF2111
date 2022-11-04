565e834a497533da9d0b04613a388ae79fd8fa03
Gibran Fasha Ghazanfar <18221069@std.stei.itb.ac.id>
2022-11-04 17:53:15 +0700

    Update commands.c

--- src/commands.c
+++ src/commands.c
@@ -24,3 +24,46 @@ void QUEUEGAME(TabWord games, Queue* game_queue) {
         printf("Nomor permainan tidak valid, silahkan masukkan nomor game pada list.\n");
     }
 }
+
+void SKIPGAME(Queue *game_queue)
+{
+    if (!isEmpty(*game_queue))
+    {
+        printf("Berikut adalah daftar Game-mu\n");
+    
+        int i;
+        int j = 1;
+        for (i = (*game_queue).idxHead; i <= (*game_queue).idxTail; i++)
+        {
+            printf("%d. %s\n", j, (*game_queue).buffer[i]);
+            j++;
+        }
+        ADVWORD();
+        int n = currentWord.TabWord[0] - '0';
+        ElTypeQueue2 gamename; //tempat sampah pas dequeue
+        if (n < length(*game_queue))
+        {
+            int i;
+            for(i = (*game_queue).idxHead; i < n; i++)
+            {
+                dequeue(game_queue,&gamename);
+            }
+
+            PLAYGAME();
+        }
+        else
+        {
+            int i;
+            for (i = (*game_queue).idxHead; i <= (*game_queue).idxTail; i++)
+            {
+                dequeue(game_queue,&gamename);
+            }
+            printf("Tidak ada permainan lagi dalam daftar game-mu.\n");
+        }
+    }
+
+    else
+    {
+        QUEUEGAME();//msh blm ngambil array game
+    }
+}
