b8e1deb440bad661544e84ca4358d49634e9a7de
WillyFFS <88759579+WillyFFS@users.noreply.github.com>
2022-11-04 17:57:18 +0700

    added list_game

---
+++ src/list_game.c
@@ -0,0 +1,23 @@
+/* File : list_game.c */
+
+
+#include "list_game.h"
+
+void list_game(TabChar list){
+    /*
+    Proses  : Membaca listglobal yang berisi nama nama game
+              yang terdapat pada file
+    I.S     : listglobal yang sudah terdefenisi
+    F.S     : Menambilkan isi dari list ke layar
+    */
+
+   //Kamus Lokal
+   int i,j;
+
+   //Algoritma
+   j = 1;
+   for(i=0;i<list.Neff;i++){
+    printf("%d. %s\n",j,list.TI[i]);
+    j +=1;
+   }
+}
---
+++ src/list_game.h
@@ -0,0 +1,18 @@
+/* File : list_game.h */
+
+#ifndef LIST_GAME_H
+#define LIST_GAME_H
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "array_list.h"
+
+void list_game (TabChar list);
+    /*
+    Proses  : Membaca listglobal yang berisi nama nama game
+              yang terdapat pada file
+    I.S     : listglobal yang sudah terdefenisi
+    F.S     : Menambilkan isi dari list ke layar
+    */
+
+#endif
