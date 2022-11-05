f2dcbb05ec495df650444a687e01f0227c5fdcb2
Fawwaz Abrial Saffa <fawwazabrial.fas@gmail.com>
2022-11-05 13:50:40 +0700

    Revert "replaced input with scan"
    This reverts commit c6e00a0ee8af883e7512ac0fb21ada7f6be244e0.

--- main.c
+++ main.c
@@ -8,25 +8,21 @@
 
 int main() {
     // KAMUS LOKAL
-    Word command1, command2;    // INPUT USER BERUPA COMMAND
-    int inputint;               // INPUT USER BERUPA INTEGER
-
-    TabWord games;              // Array untuk menyimpan game yang tersedia
-    Queue antrian_game;         // Queue untuk menyimpan antrian game ketika BNMO berjalan
-
+    Word masukan;
+    TabWord games;
+    Queue queue_game;
     boolean loop = true;
     int i, len;
 
     // ALGORITMA
-    // Mengosongkan Array untuk games dan Queue untuk antrian game
     MakeEmpty(&games);
-    CreateQueue(&antrian_game);
+    CreateQueue(&queue_game);
+    while (loop) {
+        STARTWORD();
 
-    while (loop) { // Looping menerima input untuk command SAVE dan LOAD
-        printf("ENTER COMMAND : ");
-        scan("%c %c", &command1, &command2, &inputint);
-
-        if (ValidateCommand(command1, "START")) { // INPUT ADALAH START
+        // INPUT ADALAH START
+        if (isKataEqual(currentWord, "START")) {
+            loop = false;
             STARTWORDFILE("config.txt");
             len = katatoint(currentWord);
             for (i=1; i<=len; i++) {
@@ -35,26 +31,21 @@ int main() {
                 SetEl(&games, i, currentWord);
             }
 
-            printf("File konfigurasi sistem berhasil dibaca. BNMO berhasil dijalankan.\n");
-            loop = false;
-            // DisplayArray(games);
+            // goto menu kalo udah dibuat
+            // menu(games, queue_games);
         }
 
-        else if (ValidateCommand(command1, "LOAD")) { // INPUT ADALAH LOAD
-            /* TODO : Tulis kode buat LOAD diatas sini, sebenernya tinggal 
-             * copas SAVE aja tapi di STARTWORDFILE() nama filenya diganti 
-             * sesuai masukan user, masukan nama file ada di command2 */
-            
-            printf("Save file berhasil dibaca. BNMO berhasil dijalankan.\n");
+        // INPUT ADALAH LOAD
+        else if (isKataEqual(currentWord, "LOAD")) {
+            printf("LOAD\n");
+            ADVWORD();
+            displayWord(currentWord);   // nama file yang bakal dibaca
             loop = false;
         }
 
-        else { // INPUT GAJELAS 
-            printf("Command tidak dikenali, silahkan masukkan command yang valid.\n");
+        // INPUT GAJELAS 
+        else {
+            printf("MASUKAN SALAH!\n");
         }
     }
-
-    loop = true; // Reset true untuk menu
-    /* TODO : Taro looping menu dibawah ini, dalemnya validasi tiap command1 command2 
-     * apakah sama dengan command yg tersedia, kalo iya panggil prosedur commandnya */
 }
