bfc8ab55815b3cf8a087b97eb11774b0fd730af7
Fawwaz Abrial Saffa <fawwazabrial.fas@gmail.com>
2022-11-10 11:29:06 +0700

    Updated game testing

--- bin/unit_tests/gametest.c
+++ bin/unit_tests/gametest.c
@@ -1,7 +1,22 @@
+#include "../../src/functions.h"
 #include "../../src/game_rng.h"
+#include "../../src/dinerdash.h"
 #include "../../src/game_2048.h"
 
 int main() {
-    run_2048();
+    Word c1, c2; int c3=-9999;
+
+    while (c3 != 0) {
+        printf("Masukan kode game yang ingin di-test :\n0. Quit\n1. RNG\n2. Diner Dash\n3. 2048\n");
+        scan("%d", &c1, &c2, &c3);
+
+        if (c3 == 1) {
+            run_rng(); c3=0;
+        } else if (c3 == 2) {
+            dinerdash(); c3=0;
+        } else if (c3 == 3) {
+            run_2048(); c3=0;
+        }
+    }
     return 0;
 }
