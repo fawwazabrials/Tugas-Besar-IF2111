86c19f156abb18cf15e21694d324accab7f7de96
Fawwaz Abrial Saffa <fawwazabrial.fas@gmail.com>
2022-11-10 11:28:36 +0700

    Added distictions between Queue and Dinerdash's Queue
    Nambahin postfix -DD ke semua queue di Queue Dinerdash, sama benerin header

--- src/ADT/q_dinerdash.h
+++ src/ADT/q_dinerdash.h
@@ -1,5 +1,5 @@
 /* File : q_dinerdash.h */
-/* Definisi ADT Queue dengan representasi array secara eksplisit dan alokasi statik */
+/* Definisi ADT Queue_DD dengan representasi array secara eksplisit dan alokasi statik */
 
 #ifndef Q_DINERDASH_H
 #define Q_DINERDASH_H
@@ -17,23 +17,23 @@ typedef struct {
     int harga;
 } Food;
 
-typedef Food ElType;
+typedef Food ElTypeQDD;
 
 typedef struct {
-	ElType buffer[CAPACITY]; 
-	int idxHead;
-	int idxTail;
-} Queue;
+	ElTypeQDD buffer[CAPACITY]; 
+	int idxHeadQDD;
+	int idxTailQDD;
+} Queue_DD;
 
 /* ********* AKSES (Selektor) ********* */
-/* Jika q adalah Queue, maka akses elemen : */
-#define IDX_HEAD(q) (q).idxHead
-#define IDX_TAIL(q) (q).idxTail
-#define     HEAD(q) (q).buffer[(q).idxHead]
-#define     TAIL(q) (q).buffer[(q).idxTail]
+/* Jika q adalah Queue_DD, maka akses elemen : */
+#define IDX_HEAD_DD(q) (q).idxHeadQDD
+#define IDX_TAIL_DD(q) (q).idxTailQDD
+#define     HEAD_DD(q) (q).buffer[(q).idxHeadQDD]
+#define     TAIL_DD(q) (q).buffer[(q).idxTailQDD]
 
 /* *** Kreator *** */
-void CreateQueue(Queue *q);
+void CreateQueueDD(Queue_DD *q);
 /* I.S. sembarang */
 /* F.S. Sebuah q kosong terbentuk dengan kondisi sbb: */
 /* - Index head bernilai IDX_UNDEF */
@@ -41,35 +41,35 @@ void CreateQueue(Queue *q);
 /* Proses : Melakukan alokasi, membuat sebuah q kosong */
 
 /* ********* Prototype ********* */
-boolean isEmpty(Queue q);
+boolean isEmptyDD(Queue_DD q);
 /* Mengirim true jika q kosong: lihat definisi di atas */
-boolean isFull(Queue q);
+boolean isFullDD(Queue_DD q);
 /* Mengirim true jika tabel penampung elemen q sudah penuh */
-/* yaitu IDX_TAIL akan selalu di belakang IDX_HEAD dalam buffer melingkar*/
+/* yaitu IDX_TAIL_DD akan selalu di belakang IDX_HEAD_DD dalam buffer melingkar*/
 
-int length(Queue q);
+int lengthDD(Queue_DD q);
 /* Mengirimkan banyaknya elemen queue. Mengirimkan 0 jika q kosong. */
 
 /* *** Primitif Add/Delete *** */
-void enqueue(Queue *q, ElType val);
+void enqueueDD(Queue_DD *q, ElTypeQDD val);
 /* Proses: Menambahkan val pada q dengan aturan FIFO */
 /* I.S. q mungkin kosong, tabel penampung elemen q TIDAK penuh */
-/* F.S. val menjadi TAIL yang baru, IDX_TAIL "mundur" dalam buffer melingkar. */
+/* F.S. val menjadi TAIL_DD yang baru, IDX_TAIL_DD "mundur" dalam buffer melingkar. */
 
-void dequeue(Queue *q, ElType *val);
+void dequeueDD(Queue_DD *q, ElTypeQDD *val);
 /* Proses: Menghapus val pada q dengan aturan FIFO */
 /* I.S. q tidak mungkin kosong */
-/* F.S. val = nilai elemen HEAD pd I.S., IDX_HEAD "mundur";
+/* F.S. val = nilai elemen HEAD_DD pd I.S., IDX_HEAD_DD "mundur";
         q mungkin kosong */
 
-void ForceDeleteAt(Queue *q, int i);
-/* Menghapus elemen array secara paksa (bukan dequeue) */
-/* I.S. Queue terdefinisi dan tidak kosong */
-/* F.S. Elemen Queue ke-i terhapus */
+void ForceDeleteAt(Queue_DD *q, int i);
+/* Menghapus elemen array secara paksa (bukan dequeueDD) */
+/* I.S. Queue_DD terdefinisi dan tidak kosong */
+/* F.S. Elemen Queue_DD ke-i terhapus */
 
-/* *** Display Queue *** */
-void displayQueueFood(Queue q);
-/* Proses : Menuliskan isi Queue dengan traversal */
+/* *** Display Queue_DD *** */
+void displayQueueFood(Queue_DD q);
+/* Proses : Menuliskan isi Queue_DD dengan traversal */
 /* I.S. q boleh kosong */
 /* F.S. Jika q kosong akan dicetak: */
 /*
@@ -87,8 +87,8 @@ M2      | 1              | 4         | 15000
 
 */
 
-void displayQueueRTS(Queue q);
-/* Proses : Menuliskan isi Queue dengan traversal */
+void displayQueueRTS(Queue_DD q);
+/* Proses : Menuliskan isi Queue_DD dengan traversal */
 /* I.S. q boleh kosong */
 /* F.S. Jika q tidak kosong dan memiliki 1 elemen */
 /*
@@ -96,14 +96,14 @@ Makanan | Sisa ketahanan makanan
 ---------------------------------
 M2      | 4
 */
-/* Jika Queue kosong : akan dicetak */
+/* Jika Queue_DD kosong : akan dicetak */
 /*
 Makanan | Sisa ketahanan makanan
 ---------------------------------
 */
 
-void displayQueueCooked(Queue q);
-/* Proses : Menuliskan isi Queue dengan traversal */
+void displayQueueCooked(Queue_DD q);
+/* Proses : Menuliskan isi Queue_DD dengan traversal */
 /* I.S. q boleh kosong */
 /* F.S. Jika q tidak kosong: [e1,e2,...,en] */
 /* Contoh : jika ada dua makanan yang sedang dimasak */
@@ -114,7 +114,7 @@ M0      | 1
 M1      | 3
 
 */
-/* Jika Queue kosong */
+/* Jika Queue_DD kosong */
 /*
 Makanan | Sisa durasi memasak
 -----------------------------
