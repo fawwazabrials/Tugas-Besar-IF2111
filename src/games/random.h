364b26c6d925bebc26a6398e3d598a64771d6db5
Aufar R <31300357+AufarR@users.noreply.github.com>
2022-12-01 15:04:14 +0700

    Update random game return type

--- src/games/random.h
+++ src/games/random.h
@@ -3,7 +3,7 @@
 
 #include "../functions.h"
 
-void run_random();
+int run_random();
 /* Bukan game beneran. Langsung mencetak skor yang berupa angka random ke layar.
    I.S. : Sembarang
    F.S. : Skor berupa integer random tercetak ke layar */
