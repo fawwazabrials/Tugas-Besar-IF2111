534270c381e9a5ceb9390d9aa41a4ce18684e31a
fawwazabrials <fawwazabrial.fas@gmail.com>
2022-12-02 19:12:16 +0700

    Fixed Game 2048's module calls

--- src/games/2048/2048.h
+++ src/games/2048/2048.h
@@ -4,9 +4,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include "../ADT/matriks.h"
-#include "../ADT/boolean.h"
-#include "../functions.h"
+#include "../../ADT/matriks/matriks.h"
+#include "../../ADT/boolean/boolean.h"
+#include "../../functions.h"
 
 int run_2048();
 /* Implementasi permainan 2048 dalam C menggunakan ADT Matriks.
