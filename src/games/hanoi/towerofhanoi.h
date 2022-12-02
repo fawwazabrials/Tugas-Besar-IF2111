b5243b830cbfd601562d1ccc1d7f25b1748ce4d3
fawwazabrials <fawwazabrial.fas@gmail.com>
2022-12-02 19:15:00 +0700

    Fixed Game Tower of Hanoi's module calls

--- src/games/hanoi/towerofhanoi.h
+++ src/games/hanoi/towerofhanoi.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include "../ADT/stackofhanoi.h"
+#include "../../ADT/stack/stackofhanoi.h"
 
 boolean isNumber(Word W);
 /*Mengecek apakah kata W merupakan angka*/
